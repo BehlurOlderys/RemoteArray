63b2682c58d9fd28afb6bc47b699f61c82ece399
Bartlomiej Hnatio <bartlomiej.hnatio@gmail.com>
2023-07-14 23:03:01 +0200

    Added ports diagnostics on stdout

--- app_mount.py
+++ app_mount.py
@@ -11,7 +11,10 @@ app = application = falcon.App()
 
 
 server_transaction_id_generator = DefaultServerTransactionIDGenerator()
-usb_ports = [p for p in get_available_com_ports() if "USB" in p]
+all_ports = get_available_com_ports()
+usb_ports = [p for p in all_ports if "USB" in p]
+print(f"All available ports = {all_ports}")
+print(f"USB ports = {usb_ports}")
 mount_resource = MountResource(SerialWriter(usb_ports))
 app.add_route("/mount/custom_command/{command_name}", mount_resource)
 app.add_route("/status", MountStatusResource(usb_ports))
