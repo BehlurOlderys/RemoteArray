202d7ffac306c71a3c3aecd12a16d1e97ef20175
Bartlomiej Hnatio <bartlomiej.hnatio@gmail.com>
2023-07-14 22:27:05 +0200

    Added usb ports diagnostics to status API for mount

--- app_mount.py
+++ app_mount.py
@@ -1,5 +1,5 @@
 import falcon
-from .status_resource import StatusResource
+from .status_resource import MountStatusResource
 from .mount_resource import MountResource
 from .app_utils import add_log, DefaultServerTransactionIDGenerator
 from .serial_utils import get_available_com_ports, SerialWriter
@@ -14,5 +14,5 @@ server_transaction_id_generator = DefaultServerTransactionIDGenerator()
 first_port = get_available_com_ports()[0]
 mount_resource = MountResource(SerialWriter(first_port))
 app.add_route("/mount/custom_command/{command_name}", mount_resource)
-app.add_route("/api/v1/status", StatusResource())
+app.add_route("/api/v1/status", MountStatusResource(get_available_com_ports()))
 # app.add_route("/api/v1/camera/{camera_id}/{setting_name}", camera_resource)
--- status_resource.py
+++ status_resource.py
@@ -2,6 +2,17 @@ import falcon
 import json
 
 
+class MountStatusResource:
+    def __init__(self, available_com_ports):
+        self._available = available_com_ports
+
+    # noinspection PyMethodMayBeStatic
+    def on_get(self, _req, resp):
+        resp.text = json.dumps({"server": "OK", "usb ports": self._available})
+        resp.status = falcon.HTTP_200
+        resp.content_type = falcon.MEDIA_JSON
+
+
 class StatusResource:
     # noinspection PyMethodMayBeStatic
     def on_get(self, _req, resp):
